be90f2ff2e2adfc91f3279a8445445de40289efd
abulaman8 <abulaman6@gmail.com>
2024-10-18 14:48:58 +0530

    added lockscreen script, updated i3 config, added zshrc

---
+++ .lockscreen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set the background image for i3lock
+i3lock -i ~/Pictures/wallpapers/L.jpg \
+    --scale \
+    --inside-color=00000000 \
+    --ring-color=00ff0088 \
+    --line-uses-inside \
+    --keyhl-color=00ff00ff \
+    --ringver-color=ffffff00 \
+    --separator-color=222222ff \
+    --insidever-color=00ff0000 \
+    --ringwrong-color=ff0000ff \
+    --insidewrong-color=ff000000 \
+    --verif-color=ffffffff \
+    --wrong-color=ffffffff \
+    --layout-color=ffffffff \
+    --time-color=ffffffff \
+    --date-color=ffffffff \
+    --greeter-color=ffffffff \
+    --noinput-text="" \
+    --force-clock
